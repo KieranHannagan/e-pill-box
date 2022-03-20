821bd6bbd91d39389cb222f4bf28a342f023d77e
Michael <mbahl1670@gmail.com>
2022-03-20 11:10:17 -0500

    medication page

--- client/src/pages/Medications.js
+++ client/src/pages/Medications.js
@@ -7,8 +7,19 @@ import { ADD_MEDICATION, REMOVE_MEDICATION } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const Medications = () => { 
+    const { loading, data } = useQuery(QUERY_ME);
+
+    if (loading) {
+        return <div>Loading...</div>;
+      }
+
+    if (!data.medications.length) {
+        return <h3>No Medications Yet</h3>;
+    }
+
     return (
         <main>
+            {data.medications}
             
         </main>
     );
