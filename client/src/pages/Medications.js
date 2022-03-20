5823405cf3af2871c9facd01f55d2026925cf56e
Michael <mbahl1670@gmail.com>
2022-03-20 11:50:36 -0500

    merging branches

--- client/src/pages/Medications.js
+++ client/src/pages/Medications.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Redirect, useParams } from 'react-router-dom';
 
 import { useQuery, useMutation } from '@apollo/client';
-import { QUERY_MEDICATIONS } from '../utils/queries';
+
+import { QUERY_ME } from '../utils/queries';
 import { ADD_MEDICATION, REMOVE_MEDICATION } from '../utils/mutations';
 import Auth from '../utils/auth';
 
