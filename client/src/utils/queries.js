124f72d1f0a4797d921266ec5bde9f0ade7a6b7d
Alex Wu <awu4921@gmail.com>
2022-03-20 11:35:41 -0500

    adding page references and date util

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,100 +1,48 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_THOUGHTS = gql`
-  query thoughts($username: String) {
-    thoughts(username: $username) {
-      _id
-      thoughtText
-      createdAt
-      username
-      reactionCount
-      reactions {
-        _id
-        createdAt
-        username
-        reactionBody
-      }
-    }
-  }
-`;
-
-export const QUERY_THOUGHT = gql`
-  query thought($id: ID!) {
-    thought(_id: $id) {
-      _id
-      thoughtText
-      createdAt
-      username
-      reactionCount
-      reactions {
-        _id
-        createdAt
-        username
-        reactionBody
-      }
-    }
-  }
-`;
-
-export const QUERY_USER = gql`
-  query user($username: String!) {
-    user(username: $username) {
-      _id
-      username
-      email
-      friendCount
-      friends {
-        _id
-        username
-      }
-      thoughts {
-        _id
-        thoughtText
-        createdAt
-        reactionCount
-      }
-    }
-  }
-`;
-
 export const QUERY_ME = gql`
-  {
-    me {
-      _id
-      username
-      email
-      friendCount
-      thoughts {
-        _id
-        thoughtText
-        createdAt
-        reactionCount
-        reactions {
+    query me{
+        me {
           _id
-          createdAt
-          reactionBody
           username
+          medicationCount
+          medications {
+              _id
+              drugName
+              pharmacyName
+              pharmacyPhone
+              daySupply
+              lastFill
+            }
         }
-      }
-      friends {
-        _id
-        username
-      }
     }
-  }
 `;
 
-export const QUERY_ME_BASIC = gql`
-  {
-    me {
-      _id
-      username
-      email
-      friendCount
-      friends {
-        _id
-        username
-      }
+export const QUERY_MEDICATIONS = gql`
+    query medications($username: String) {
+        medications(username: $username) {
+          _id
+          username
+          medicationCount
+          medications {
+              _id
+              drugName
+              pharmacyName
+              pharmacyPhone
+              daySupply
+              lastFill
+            }
+        }
     }
-  }
+`;
+
+export const QUERY_MEDICATION = gql`
+    query medication($id: ID!) {
+        _id
+        drugName
+        pharmacyName
+        pharmacyPhone
+        daySupply
+        lastFill
+    }
 `;
